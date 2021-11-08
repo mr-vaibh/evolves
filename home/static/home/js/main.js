92d6ababf76e49043d73fcd80aaa3dcf30d0f444
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-11-08 17:07:40 +0530

    shortcut key for searchbar

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -13,6 +13,15 @@ $(document).ready(function () {
         }
     });
 
+    // shortcut key to search
+    document.addEventListener('keyup', (e) => {
+        // this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
+        if (e.ctrlKey && e.key === '/') {
+            // call your function to do the thing
+            searchField.focus();
+        }
+    }, false);
+
 
     // To update localstorage
     get_cart();
